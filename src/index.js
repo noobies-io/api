70ad1fecdf286900047fd1756f75c4e749d7db40
Ulisses Ferreira <ulissesvf@gmail.com>
2019-06-24 00:32:13 +0100

    feat: application entrypoint moved + new functionality

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// Get environment variables
+require('dotenv').config()
+
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: false })
+
+// Register our DynamoDB interface
+fastify.register(require('./services/database'))
+
+// Register CORS handler
+fastify.register(require('fastify-cors'), {
+  // put your options here
+  origin: '*'
+})
+
+fastify.register(require('fastify-cookie'))
+
+// Routes handled in different JSs
+const positions = require('./routes/positions')
+const auth = require('./routes/auth')
+
+// Register route handlers
+fastify.register(auth, { prefix: '/auth' })
+fastify.register(positions, { prefix: '/positions' })
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(process.env.PORT)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
