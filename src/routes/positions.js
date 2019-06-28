f9f923515c452390fb5cd7c68f7310aa9dd91505
Ulisses Ferreira <ulissesvf@gmail.com>
2019-06-28 03:11:50 +0100

    feat: filter positions + cache is now standard and needs to be cleared manually

--- src/routes/positions.js
+++ src/routes/positions.js
@@ -1,17 +1,72 @@
-const fetch = require("node-fetch");
 const Airtable = require("../services/airtable")
 
 module.exports = function (fastify, opts, next) {
 
-  this.a = new Airtable()
+  this.airtableService = new Airtable()
 
+  /**
+   * Get all positions (bandwidth intensive)
+   */
   fastify.get('/', async (request, reply) => {
-    try {
-      let json = await this.a.getPositions()
-      return json
-    } catch (error) {
-      console.log(error)
-      return { error: 'Error' }
+    return this.airtableService.getPositions()
+  })
+
+  /**
+   * Get the positions displayed in the main page
+   */
+  fastify.get('/main', async (request, reply) => {
+    return this.airtableService.getHotPositions()
+  })
+
+  /**
+   * Get positions filtered with query details
+   * 
+   * @body ?category - String identifying the category (Example: "Science")
+   * @body ?duration - String with the duration (Example: "3 meses")
+   * @body ?location - String with the location (Example: "Lisbo")
+   * @body ?pay - Any value. If none is present all results are shown. If
+   * any value is present we will assume you want payed internships.
+   * @body ?degree - Any value. If none is present all results are shown.
+   * If any value is present we will return positions that need a graduate. 
+   * @body ?text - String with a company or title name. Both will be checked
+   */
+  fastify.get('/filter', async (request, reply) => {
+
+    const category = request.query.category
+    const duration = request.query.duration
+    const location = request.query.location
+    const pay = request.query.pay
+    const degree = request.query.requirement
+    const textSearch = request.query.text.toLowerCase()
+
+    const positions = await this.airtableService.getPositions()
+
+    const filteredPositions = positions.filter((item) => {
+      return (category ? item.category === category : true) &&
+        (duration ? item.duration === duration : true) &&
+        (location ? item.location === location : true) &&
+        (pay ? item.payment !== 0 : true) &&
+        (degree ? item.degree === 'yes' : true) &&
+        (textSearch ? (
+          (textSearch ? item.company.toLowerCase().includes(textSearch) : false) ||
+          (textSearch ? item.title.toLowerCase().includes(textSearch) : false)
+        ) : true)
+    })
+  
+    reply.code(200).send(filteredPositions)
+  })
+
+  /**
+   * Cancel cache and fetch new data from Airtable
+   */
+  fastify.get('/update', async (request, reply) => {
+
+    let success = await this.airtableService.updatePositions()
+
+    if (success) {
+      reply.code(200).send({ ok: true })
+    } else {
+      reply.code(500).send({ ok: false })
     }
   })
 
--- src/services/airtable.js
+++ src/services/airtable.js
@@ -1,36 +1,60 @@
 const fetch = require("node-fetch");
 
-const EXPIRES = 60 * 1000;
+//const EXPIRES = 60 * 1000;
 
 module.exports = class Airtable {
 
+  /**
+   * This will store all the positions avaliable and organize them.
+   */
   constructor() {
-    console.log('Started service')
-    this.cache = null
-    this.cached = false
-    this.time = null
-
-    this.getPositions()
+    this.positions = null
+    this.hot = null
+    this.updatePositions()
   }
 
-  async getPositions() {
-    if (this.cached && this.time > Date.now() - EXPIRES) {
-      console.log('Cache hit!')
-      return this.cache;
-    } else {
-      console.log('Cache miss :(')
+  getPositions() {
+    return this.positions
+  }
+
+  getHotPositions() {
+    return this.hot
+  }
+
+  async updatePositions() { // TODO - Implement hot mechanism
+    try {
       const response = await fetch('https://api.airtable.com/v0/appIzularDDL2q6Mw/Main', {
         method: 'get',
         headers: {
           'Authorization': `Bearer ${process.env.AIRTABLE_API_KEY}`
         }
       })
-      this.cache = await response.json()
-      this.cached = true
-      this.time = Date.now()
 
-      return this.cache
+      const airtableData = await response.json()
+
+      this.positions = airtableData.records.map((entry) => {
+        console.log(entry.fields)
+        let newEntry = {
+          title: entry.fields.Title,
+          category: entry.fields.Category,
+          company: entry.fields.Company,
+          smallLogo: entry.fields.Logo[0].thumbnails.large.url,
+          bigLogo: entry.fields.Logo[0].thumbnails.full.url,
+          duration: entry.fields.Duration,
+          location: entry.fields.Title,
+          description: entry.fields.Description,
+          requirements: entry.fields.Requirements,
+          url: entry.fields['URL'],
+          date: entry.fields.Date,
+          payment: entry.fields.Payment
+        }
+
+        return newEntry
+      })
+
+      return true
+    } catch (err) {
+      return false
     }
   }
-
 }
